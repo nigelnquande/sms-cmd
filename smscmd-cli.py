37839630019d0318ad23555927bb8fa8e1c18c67
Nigel Nquande <nigel.nquande@gmail.com>
2017-03-15 19:30:18 +0200

    Update smscmd-cli.py

--- smscmd-cli.py
+++ smscmd-cli.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Read http://www.souravsengupta.com/int2pro2014/python/LPTHW.pdf and
+# http://stock.ethop.org/pdf/python/Learning%20Python,%205th%20Edition.pdf for getting up to spead with Python
+# See https://xkcd.com/353/ for why you should.
+
 # Command-line client of SMS CMD
 
 from sys import argv, stderr, stdout, exit
@@ -8,7 +12,7 @@ try:
   """Import libs necessary to talk to the server, which always runs and keeps track of msgs sent per day, 
     so as to limit abuse/spam to 100 max."""
 except ImportError:
-  stderr.write("Could not import twilio.com REST API. Please install it.")
+  stderr.write("Could not import module X. Please install it.")
   
 
 
@@ -16,6 +20,7 @@ except ImportError:
 
 def main():
   # Mainline logic happens here
+  # do some arsing of command line arguments
 
 if __name__ == "__main__":
   main()
